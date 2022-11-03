8b3c0dde76ecf51843a02cebbe0fbf83ba17445a
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-11-02 21:30:17 -0300

    diagrama Prisma, contador boloes

--- server/src/server.js
+++ server/src/server.js
@@ -4,12 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient({
+    log: ['query'],
+});
 async function bootstrap() {
     const fastify = (0, fastify_1.default)({
         logger: true,
     });
-    fastify.get('/pools/count', () => {
-        return { count: 0 };
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count();
+        return { count };
     });
     await fastify.listen({ port: 3333 });
 }
