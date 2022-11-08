7a1c8bbf11ca8526e24f44a0eb89abea96072567
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-11-08 09:21:56 -0300

    Interface SignIn Ok

--- mobile/screens/signin.tsx
+++ mobile/screens/signin.tsx
@@ -1,10 +1,24 @@
-import { Center, Text } from 'native-base'
-import Logo from '../assets/logo.svg'
+import { Center, Text, Icon } from 'native-base'
+import { Button } from '../components/Button'
+import Logo from '../src/assets/logo.svg'
+import { Fontisto } from '@expo/vector-icons'
 
 export function SignIn(){
     return (
-      <Center flex={1} bgColor="gray.900">
-        <Logo></Logo>
+      <Center flex={1} bgColor="gray.900" p={7}>
+        <Logo width={212} height={40}/>
+
+        <Button 
+          title='ENTRAR COM GOOGLE'
+          type='SECUNDARY'
+          leftIcon={<Icon as={Fontisto} name='google' color='white' size='md'/>}
+          mt={12}
+        />
+
+        <Text fontSize={12} color='white' textAlign='center' mt={4}>
+          Não utilizamos nenhuma informação além {'\n'}
+          do seu e-mail para criação de sua conta.
+        </Text>
       </Center>
     )
 }
